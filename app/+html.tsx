f5ec61d2d28c11902066452cb7b0e774c814d3da
katawutp <katawut.paramee@gmail.com.com>
2025-02-02 17:22:58 +0700

    meta charSet=UTF-8

--- app/+html.tsx
+++ app/+html.tsx
@@ -5,7 +5,7 @@ export default function Root({ children }: PropsWithChildren) {
  return (
     <html lang="en">
 <head>
-    <meta charset="UTF-8" />
+    <meta charSet="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Document</title>
 </head>
