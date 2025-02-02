2728641a021f85ef5b5bf8bfeb32fbac3b84694d
katawutp <katawut.paramee@gmail.com.com>
2025-02-02 22:30:44 +0700

    style instead of className nativewind

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,23 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello, NativeWind!</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'black',
+  },
+  text: {
+    color: 'white',
+    fontSize: 24, // text-3xl (24px in Tailwind)
+    fontWeight: 'bold', // font-bold
+  },
+});
