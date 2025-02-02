bc12df73dde2665053c771d2143465816d3effa3
katawutp <katawut.paramee@gmail.com.com>
2025-02-02 17:18:38 +0700

    +html.tsx

---
+++ app/+html.tsx
@@ -0,0 +1,17 @@
+import { ScrollViewStyleReset } from 'expo-router/html';
+import { type PropsWithChildren } from 'react';
+
+export default function Root({ children }: PropsWithChildren) {
+ return (
+    <html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+ )    
+}
